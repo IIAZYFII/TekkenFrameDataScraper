2b33349e86ec2cbb360875cc55ea977682451104
Hussain Asif <54109830+IIAZYFII@users.noreply.github.com>
2022-09-04 15:04:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,32 +20,30 @@ for character_elem in character_elems:
         links.append(character_elem.get_attribute('href'))
 
 for i in range(len(links)):
-    if (i >= 8):
-
-        driver.get(links[i])
-        driver.implicitly_wait(20)
-        character_name  = driver.find_element(By.CLASS_NAME,"title").text
-        table = driver.find_element(By.TAG_NAME, "table")
-        rows = table.find_elements(By.CSS_SELECTOR, 'tr.mat-row.cdk-row.ng-star-inserted')
-        with open(character_name + '.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Command', 'Hit Level', 'Damage', 'Startup', 'Block', 'Hit', 'Counter Hit', 'Notes'])
-            print(character_name)
-            for row in rows:
-                container = row.find_element(By.CLASS_NAME, "move-container")
-                command = container.find_element(By.TAG_NAME, "div")
-                hit_level = row.find_element(By.CSS_SELECTOR, 'td.mat-cell.cdk-cell.cdk-column-hitLevel.mat-column-hitLevel.ng-star-inserted')
-                damage = row.find_element(By.CSS_SELECTOR, 'td.mat-cell.cdk-cell.cdk-column-damage.mat-column-damage.ng-star-inserted')
-                startup = row.find_element(By.CSS_SELECTOR,'td.mat-cell.cdk-cell.cdk-column-startup.mat-column-startup.ng-star-inserted')
-                block = row.find_element(By.CSS_SELECTOR,'td.mat-cell.cdk-cell.cdk-column-block.mat-column-block.ng-star-inserted')
-                hit = row.find_element(By.CSS_SELECTOR,
+    driver.get(links[i])
+    driver.implicitly_wait(20)
+    character_name  = driver.find_element(By.CLASS_NAME,"title").text
+    table = driver.find_element(By.TAG_NAME, "table")
+    rows = table.find_elements(By.CSS_SELECTOR, 'tr.mat-row.cdk-row.ng-star-inserted')
+    with open(character_name + '.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Command', 'Hit Level', 'Damage', 'Startup', 'Block', 'Hit', 'Counter Hit', 'Notes'])
+        print(character_name)
+        for row in rows:
+            container = row.find_element(By.CLASS_NAME, "move-container")
+            command = container.find_element(By.TAG_NAME, "div")
+            hit_level = row.find_element(By.CSS_SELECTOR, 'td.mat-cell.cdk-cell.cdk-column-hitLevel.mat-column-hitLevel.ng-star-inserted')
+            damage = row.find_element(By.CSS_SELECTOR, 'td.mat-cell.cdk-cell.cdk-column-damage.mat-column-damage.ng-star-inserted')
+            startup = row.find_element(By.CSS_SELECTOR,'td.mat-cell.cdk-cell.cdk-column-startup.mat-column-startup.ng-star-inserted')
+            block = row.find_element(By.CSS_SELECTOR,'td.mat-cell.cdk-cell.cdk-column-block.mat-column-block.ng-star-inserted')
+            hit = row.find_element(By.CSS_SELECTOR,
                                    'td.mat-cell.cdk-cell.cdk-column-hit.mat-column-hit.ng-star-inserted')
-                counter_hit = row.find_element(By.CSS_SELECTOR,
+            counter_hit = row.find_element(By.CSS_SELECTOR,
                                    'td.mat-cell.cdk-cell.cdk-column-counterHit.mat-column-counterHit.ng-star-inserted')
-                notes = row.find_element(By.CSS_SELECTOR,'td.mat-cell.cdk-cell.cdk-column-notes.mat-column-notes.ng-star-inserted')
-                writer.writerow([command.text, hit_level.text, damage.text, startup.text, block.text, hit.text,
+            notes = row.find_element(By.CSS_SELECTOR,'td.mat-cell.cdk-cell.cdk-column-notes.mat-column-notes.ng-star-inserted')
+            writer.writerow([command.text, hit_level.text, damage.text, startup.text, block.text, hit.text,
                              counter_hit.text, notes.text])
-                print(command.text, hit_level.text, damage.text, startup.text, block.text, hit.text,
+            print(command.text, hit_level.text, damage.text, startup.text, block.text, hit.text,
                              counter_hit.text, notes.text)
 
 
